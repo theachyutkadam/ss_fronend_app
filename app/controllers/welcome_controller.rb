e27f384093b134f80905b29b5d2df2aa6fd0ed94
achyut kadam <theachyutkadam@gmail.com>
2022-06-14 20:20:14 +0530

    Show login logout page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,16 +1,18 @@
 class WelcomeController < ApplicationController
   def index
-    redirect_to welcome_sign_in_path unless cookies[:is_login]
+    redirect_to welcome_login_path unless cookies[:is_login]
   end
 
+  def login; end
+
   def sign_in
+    return redirect_to welcome_login_path if params[:email].blank? || params[:password].blank?
     payload = {email: params[:email], password: params[:password]}
     url = "http://localhost:3000/authenticate"
     response = RestClient.post(url, payload, headers={})
     if response.body
       cookies[:is_login] = true
-      redirect_to welcome_index_path
+      redirect_to root_path
     end
-    redirect_to welcome_sign_in_path
   end
 end
